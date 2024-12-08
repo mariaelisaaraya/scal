07b74e9055dc86e212e4a0093cceae19e5cae95d
mariaelisaaraya <arayamariaelisaa@gmail.com>
2024-12-07 21:54:18 -0300

    visual

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,29 +11,29 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow pt-10 bg-red-100">
         <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">WELCOME TO</span>
+          <h1 className="text-center text-blue-600">
+            <span className="block text-2xl mb-2">BIENVENIDO A</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
+            <p className="my-2 font-medium">Dirección Conectada:</p>
             <Address address={connectedAddress} />
           </div>
 
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Comienza editando{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/nextjs/app/page.tsx
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Edita tu contrato inteligente{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               YourContract.sol
             </code>{" "}
-            in{" "}
+            en{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/hardhat/contracts
             </code>
@@ -45,21 +45,21 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Experimenta con tu contrato inteligente usando la pestaña{" "}
                 <Link href="/debug" passHref className="link">
-                  Debug Contracts
+                  Depurar Contratos
                 </Link>{" "}
-                tab.
+                .
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                Explora tus transacciones locales con la pestaña{" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                  Explorador de Bloques
                 </Link>{" "}
-                tab.
+                .
               </p>
             </div>
           </div>
